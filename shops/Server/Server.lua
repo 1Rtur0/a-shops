e28b1f1878c3d46c99d31bf09524b02216ab5fbd
ARtuRO <71898120+1Rtur0@users.noreply.github.com>
2022-08-11 19:02:28 -0500

    Update Server.lua

--- shops/Server/Server.lua
+++ shops/Server/Server.lua
@@ -1,87 +1,87 @@
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-print('^0[^4ARtuRO Badulaque^0] ^2Starteado^0')
+print('^0[^4ARtuRO Shops^0] ^2Starteado^0')
 
-ESX.RegisterServerCallback('badulaques:ComprarOtros', function(source, cb, weaponName,identifier)
+ESX.RegisterServerCallback('shops:ComprarOtros', function(source, cb, weaponName,identifier)
 	local xPlayer = ESX.GetPlayerFromId(source)
     local fullname = GetPlayerName(source)
     local id = ESX.GetPlayerFromId(source).getIdentifier()
     local DATE = os.date("%H:%M:%S %d/%m/%y")
-	local price = Config.ItemsOtros[identifier].price
+	local price = Config.ItemsOthers[identifier].price
     local webhook = Config.Webhook
 		
 			if xPlayer.getMoney() >= price then
 				xPlayer.removeMoney(price)
 				xPlayer.addInventoryItem(weaponName, 1)
-				xPlayer.showNotification('Has comprado 1x '..weaponName..' por '..price..'$')
-			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "arturo's badulaque", content = "**Badulaque**\n```Nombre:" ..fullname.. "\nSteam:"..xPlayer.identifier.."\nComprado:"..weaponName.."\nHora:"..DATE.."```"}), { ['Content-Type'] = 'application/json' })
+				xPlayer.showNotification('You have bought 1x a '..weaponName..' for '..price..'$')
+			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "arturo's shops", content = "**Shops**\n```Name:" ..fullname.. "\nSteam:"..xPlayer.identifier.."\nBought:"..weaponName.."\Time:"..DATE.."```"}), { ['Content-Type'] = 'application/json' })
 				cb(true)
 			else
-				xPlayer.showNotification('No tienes suficiente dinero')
+				xPlayer.showNotification('You dont have enough money')
 				cb(false)
 			end
 			
 end)
 
-ESX.RegisterServerCallback('badulaques:ComprarBebidas', function(source, cb, weaponName,identifier)
+ESX.RegisterServerCallback('shops:ComprarBebidas', function(source, cb, weaponName,identifier)
 	local xPlayer = ESX.GetPlayerFromId(source)
     local fullname = GetPlayerName(source)
     local id = ESX.GetPlayerFromId(source).getIdentifier()
     local DATE = os.date("%H:%M:%S %d/%m/%y")
-	local price = Config.ItemsBebidas[identifier].price
+	local price = Config.ItemsDrinks[identifier].price
     local webhook = Config.Webhook
 		
 			if xPlayer.getMoney() >= price then
 				xPlayer.removeMoney(price)
 				xPlayer.addInventoryItem(weaponName, 1)
-				xPlayer.showNotification('Has comprado 1x '..weaponName..' por '..price..'$')
-			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "arturo's badulaque", content = "**Badulaque**\n```Nombre:" ..fullname.. "\nSteam:"..xPlayer.identifier.."\nComprado:"..weaponName.."\nHora:"..DATE.."```"}), { ['Content-Type'] = 'application/json' })
+				xPlayer.showNotification('You have bought 1x a '..weaponName..' for '..price..'$')
+			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "arturo's shops", content = "**Shops**\n```Name:" ..fullname.. "\nSteam:"..xPlayer.identifier.."\nBought:"..weaponName.."\Time:"..DATE.."```"}), { ['Content-Type'] = 'application/json' })
 				cb(true)
 			else
-				xPlayer.showNotification('No tienes suficiente dinero')
+				xPlayer.showNotification('You dont have enough money')
 				cb(false)
 			end
 			
 end)
 
-ESX.RegisterServerCallback('badulaques:ComprarSnacks', function(source, cb, weaponName,identifier)
+ESX.RegisterServerCallback('shops:ComprarSnacks', function(source, cb, weaponName,identifier)
 	local xPlayer = ESX.GetPlayerFromId(source)
     local fullname = GetPlayerName(source)
     local id = ESX.GetPlayerFromId(source).getIdentifier()
     local DATE = os.date("%H:%M:%S %d/%m/%y")
 	local price = Config.ItemsSnacks[identifier].price
-    local webhook = Config.webhook
-		
-			if xPlayer.getMoney() >= price then
-				xPlayer.removeMoney(price)
-				xPlayer.addInventoryItem(weaponName, 1)
-				xPlayer.showNotification('Has comprado 1x '..weaponName..' por '..price..'$')
-			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "arturo's badulaque", content = "**Badulaque**\n```Nombre:" ..fullname.. "\nSteam:"..xPlayer.identifier.."\nComprado:"..weaponName.."\nHora:"..DATE.."```"}), { ['Content-Type'] = 'application/json' })
-				cb(true)
-			else
-				xPlayer.showNotification('No tienes suficiente dinero')
-				cb(false)
-			end
-			
-end)
-
-ESX.RegisterServerCallback('badulaques:ComprarComidaLista', function(source, cb, weaponName,identifier)
-	local xPlayer = ESX.GetPlayerFromId(source)
-    local fullname = GetPlayerName(source)
-    local id = ESX.GetPlayerFromId(source).getIdentifier()
-    local DATE = os.date("%H:%M:%S %d/%m/%y")
-	local price = Config.ItemsComidaLista[identifier].price
     local webhook = Config.Webhook
 		
 			if xPlayer.getMoney() >= price then
 				xPlayer.removeMoney(price)
 				xPlayer.addInventoryItem(weaponName, 1)
-				xPlayer.showNotification('Has comprado 1x '..weaponName..' por '..price..'$')
-			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "arturo's badulaque", content = "**Badulaque**\n```Nombre: " ..fullname.. "\nSteam: "..xPlayer.identifier.."\nComprado: "..weaponName.."\nHora: "..DATE.."```"}), { ['Content-Type'] = 'application/json' })
+				xPlayer.showNotification('You have bought 1x a '..weaponName..' for '..price..'$')
+			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "arturo's shops", content = "**Shops**\n```Name:" ..fullname.. "\nSteam:"..xPlayer.identifier.."\nBought:"..weaponName.."\Time:"..DATE.."```"}), { ['Content-Type'] = 'application/json' })
 				cb(true)
 			else
-				xPlayer.showNotification('No tienes suficiente dinero')
+				xPlayer.showNotification('You dont have enough money')
+				cb(false)
+			end
+			
+end)
+
+ESX.RegisterServerCallback('shops:ComprarComidaLista', function(source, cb, weaponName,identifier)
+	local xPlayer = ESX.GetPlayerFromId(source)
+    local fullname = GetPlayerName(source)
+    local id = ESX.GetPlayerFromId(source).getIdentifier()
+    local DATE = os.date("%H:%M:%S %d/%m/%y")
+	local price = Config.ItemsReadyMeal[identifier].price
+    local webhook = Config.Webhook
+		
+			if xPlayer.getMoney() >= price then
+				xPlayer.removeMoney(price)
+				xPlayer.addInventoryItem(weaponName, 1)
+				xPlayer.showNotification('You have bought 1x a '..weaponName..' for '..price..'$')
+			PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "arturo's shops", content = "**Shops**\n```Name:" ..fullname.. "\nSteam:"..xPlayer.identifier.."\nBought:"..weaponName.."\Time:"..DATE.."```"}), { ['Content-Type'] = 'application/json' })
+				cb(true)
+			else
+				xPlayer.showNotification('You dont have enough money')
 				cb(false)
 			end
 			
